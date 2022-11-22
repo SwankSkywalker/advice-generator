782f83c3b74ab73039ec8c95b5471ba02cc7767c
Swank <willmadedesign@gmail.com>
2022-11-22 14:30:24 -0800

    refactor json parsing thanks to @theurbandev

--- .src/main.js
+++ .src/main.js
@@ -31,19 +31,43 @@
 //     quoteElement.textContent = `${apiUrl}/advice/${slip.advice}`
 // }
 
-// const rollDice = document.querySelector('.dice');
 // const adviceQuote = document.getElementById('adviceQuote');
 // const slipNum = document.querySelector('.adv-num');
 
-const fetchAdvice = fetch('https://api.adviceslip.com/advice');
+// const fetchAdvice = fetch('https://api.adviceslip.com/advice');
 
-fetchAdvice
-    .then((Response) => {
-        if (!Response.ok) {
-            throw new Error(`HTTP error: ${Response.status}`);
-        }
-        return Response.json();
-    })
-    .then((data) => {
-        console.log(data)
-    });
+// fetchAdvice
+//     .then((Response) => {
+    //         if (!Response.ok) {
+        //             throw new Error(`HTTP error: ${Response.status}`);
+        //         }
+        //         return Response.json();
+        //     })
+        //     .then((data) => {
+            //         console.log(data)
+            //     });
+            
+const rollDice = document.querySelector('.dice');
+            
+async function genNewAdvice() {
+    const requestURL = 'https://api.adviceslip.com/advice';
+    const request = new Request(requestURL);
+
+    const adviceSlip = await fetch(request);
+    const advice = await adviceSlip.json();
+
+    console.log(advice);
+    newAdviceContent(advice);
+}
+
+function newAdviceContent(advice) {
+    const id = document.querySelector('.adv-num');
+    const quote = document.querySelector('.quote');
+
+    console.log(advice.slip);    
+
+    id.textContent = `ADVICE #${advice.slip.id}`;
+    quote.innerHTML = `"${advice.slip.advice}"`;
+}
+
+rollDice.addEventListener('click', genNewAdvice());
